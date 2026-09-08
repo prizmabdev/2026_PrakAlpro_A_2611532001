03eab21282701064f74d8f06e4d06a21936bef6d
gilangadaada <gilangsalahan@gmail.com>
2026-09-08 09:06:03 +0700

    add program String_2611532001.py

---
+++ pekan2/program2/String_2611532001.py
@@ -0,0 +1,16 @@
+# nama variabel ditambah 4 digit nim terakhir contoh: nama_1234
+#ganti programnya dan data masing masing
+
+nama_2001 = "Rizki Ramadhani Al Michtari"
+jenis_kelamin_2001 = 'L'
+alamat_2001 = """
+    Asrama Hijau Universitas Andalas
+    Kecamatan Pauh
+    Kota Padang
+"""
+agama_2001 = "Islam"
+
+print("Nama saya adalah: ", nama_2001)
+print("Jenis kelamin saya adalah: ", jenis_kelamin_2001)
+print("Alamat saya adalah: ", alamat_2001)
+print("Agama saya adalah: ", agama_2001)
