bd6e37b3ac7fbf98eff3a97fd1a891fdf3dd31fd
prizmabdev <prizmabdev@gmail.com>
2026-09-10 10:03:29 +0700

    update laprak2

--- pekan2/program2/Numerik_2611532001.py
+++ pekan2/program2/Numerik_2611532001.py
@@ -2,10 +2,10 @@
 a_2001 = 100
 print("The type of variable having value", a_2001,"is", type(a_2001))
 
-# Create a variable with integer value
+# Create a variable with float value
 b_2001 = 10.2345
 print("The type of variable having value", b_2001,"is", type(b_2001))
 
-# Create a variable with integer value
+# Create a variable with complex value
 c_2001 = 100+3j
 print("The type of variable having value", c_2001,"is", type(c_2001))
